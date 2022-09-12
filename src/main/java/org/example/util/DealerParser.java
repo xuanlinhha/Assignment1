f4cc1d0812b8f172417c1770a099ea63a529fee0
Xuan Linh Ha <xuanlinhha@Linh-M1Pro.local>
2022-09-12 22:06:44 +0700

    Update

--- src/main/java/org/example/util/DealerParser.java
+++ src/main/java/org/example/util/DealerParser.java
@@ -16,8 +16,7 @@ public class DealerParser {
         CSVFormat format = CSVFormat.Builder.create().setDelimiter('|').setHeader("ID", "Name", "Address", "Phone", "Status").setSkipHeaderRecord(true).build();
         Iterable<CSVRecord> records = format.parse(in);
         for (CSVRecord record : records) {
-            String statusS = record.get(4).toUpperCase();
-            Status status = Status.valueOf(statusS);
+            Status status = Status.valueOf(record.get(4).toUpperCase());
             Dealer dealer = new Dealer(record.get(0), record.get(1), record.get(2), record.get(3), status);
             dealers.add(dealer);
         }
